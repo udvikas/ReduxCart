4487d0ccebe186285a599fe584960e708fd256d2
Vikash Sharma <kvikashsharma09@gmail.com>
2023-07-09 16:37:03 +0530

    Add to Cart Fixes

--- redux-cart/src/store/index.jsx
+++ redux-cart/src/store/index.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 const store = configureStore({
     reducer: {
-        ui: uiSlice
+        ui: uiSlice,
+        cart: cartSlice
     }
 })
 export default store;
