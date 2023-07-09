ad0dd5fb69a5dbdb3e24756caba950008d7ccccd
Vikash Sharma <kvikashsharma09@gmail.com>
2023-07-09 23:36:47 +0530

    Doing Network calls and andling response the right way

--- redux-cart/src/store/ui-slice.jsx
+++ redux-cart/src/store/ui-slice.jsx
@@ -1,13 +1,20 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-    name: 'UI',
-    initialState: { cartIsVisible: false },
-    reducers: {
-        toggle(state) {
-            state.cartIsVisible = !state.cartIsVisible
-        },
+  name: "UI",
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 export const uiActions = uiSlice.actions;
 export default uiSlice.reducer;
